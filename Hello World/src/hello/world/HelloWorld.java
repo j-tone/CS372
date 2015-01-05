03b98d02f07eef25592bff125038758ec7d19ee0
j-tone <jtoney17@my.whitworth.edu>
2015-01-05 15:42:40 -0800

    Second Commit
    Played around with "score" and favorite color

--- Hello World/src/hello/world/HelloWorld.java
+++ Hello World/src/hello/world/HelloWorld.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package hello.world;
 
+package hello.world;
+import java.util.Scanner;
 /**
  *
  * @author Justin
@@ -15,7 +16,16 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String name = "Justin";
+        System.out.printf("Hello %s, what was your score? ", name);
+        Scanner input = new Scanner(System.in);
+        int score = input.nextInt();
+        System.out.printf("Your score is %d\n", score);
+        
+        System.out.printf("And what is your favorite color? ");
+        Scanner input1 = new Scanner(System.in);
+        String color = input1.nextLine();
+        System.out.printf("Your favorite color is: %s\n", color);
     }
     
 }
